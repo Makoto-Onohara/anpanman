924a375c2afb622b70e95dbc647bb729e61a6783
大野原信 <onoharamakoto@mac-mako.local>
2020-08-26 00:36:02 +0900

    arranged the anpanman.blade.php code

--- resources/views/game/anpanman.blade.php
+++ resources/views/game/anpanman.blade.php
@@ -1,17 +1,13 @@
 @extends('layout')
 @section('title', '神経衰弱')
 @section('content')
-        <div id="msg">
-        カードを選択してください
-        </div>
-
-
-        <ul class="image_list">
-            @foreach($characters as $character)
-                <li class="cardRoom"><img class="gameCardImage unselected" src="/storage/{{ $character->image_path }}" alt=""></li>
-            @endforeach
-
-        </ul>
-    <script src="/js/anpanmangame.js"></script>
-
+<div id="msg">
+カードを選択してください
+</div>
+<ul class="image_list">
+    @foreach($characters as $character)
+        <li class="cardRoom"><img class="gameCardImage unselected" src="/storage/{{ $character->image_path }}" alt=""></li>
+    @endforeach
+</ul>
+<script src="/js/anpanmangame.js"></script>
 @endsection
