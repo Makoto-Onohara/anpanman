dca9924c81267ba00fd84084045df19c4bf3abd3
大野原信 <onoharamakoto@mac-mako.local>
2020-08-28 19:36:21 +0900

    add login

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,19 +1,32 @@
 <header>
-
-<nav class="nav">
-    <h1>アンパンマンの神経衰弱ゲーム</h1>
-    <ul>
-        <a href="{{ url('/') }}"><li id="noborder">トップページ(カード一覧）</li></a>
-        <a href="{{ route('game') }}"><li>アンパンマン</li></a>
-        <a class="nav-link" href="#"><li>車</li></a>
-        <a class="nav-link" href="#"><li>山</li></a>
-        <a class="nav-link" href="#"><li>花</li></a>
-        <a class="nav-link" href="/anpanman/uploadForm"><li>画像のアップロード</li></a>
-    </ul>
-
-
-
-
-
-</nav>
+@if(isset(Auth::user()->name))
+    <nav class="nav">
+        <ul class=nav-right>
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <li>名前：{{ Auth::user()->name }}</li>
+            </a>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+            <li>{{ 'ログアウト' }}</li>
+            </a>
+        
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </ul>
+        <br>
+        <h1>アンパンマンの神経衰弱ゲーム</h1>
+        <ul>
+            <a href="{{ url('/') }}"><li class="gamemenu" id="noborder">トップページ(カード一覧）</li></a>
+            <a href="{{ route('game') }}"><li class="gamemenu">アンパンマン</li></a>
+            <a class="nav-link" href="#"><li class="gamemenu">車</li></a>
+            <a class="nav-link" href="#"><li class="gamemenu">山</li></a>
+            <a class="nav-link" href="#"><li class="gamemenu">花</li></a>
+            <a class="nav-link" href="/anpanman/uploadForm"><li class="gamemenu">画像のアップロード</li></a>
+        </ul>
+    </nav>
+@else
+    <script>window.location = "/";</script>
+@endif
 </header>
--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/', 'HomeCotroller@index')-name('loginview');
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
 
-Route::get('/', 'gameController@showTop')->name('top');
+Route::get('/home', 'gameController@showTop')->name('home');
 Route::get('/anpanman/uploadForm', 'uploadController@uploadForm')->name('uploadForm');
 Route::post('/anpanman/upload', 'uploadController@upload')->name('upload');
 Route::get('/anpanman', 'uploadController@showCharacter')->name('show');
 
 Route::get('/anpanman/game', 'gameController@anpanmanGame')->name('game');
+
+// Route::get('/home', 'HomeController@index')->name('home');
