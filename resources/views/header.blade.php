32d04666f2ac56f350a006cb984f61a84ea2c1ce
大野原信 <onoharamakoto@mac-mako.local>
2020-08-30 18:03:49 +0900

    changed some css files and web route and blade files

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,29 +1,34 @@
 <header>
 @if(isset(Auth::user()->name))
-    <nav class="nav">
-        <ul class=nav-right>
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <li>名前：{{ Auth::user()->name }}</li>
-            </a>
-            <a class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
+<nav class="nav">
+    <div class="flex">
+        <div class="title">
+            <h1 class="media-font-15">アンパンマンの神経衰弱ゲーム</h1>
+        </div>
+        <div class="Auths">
+            <ul class="nav-right flex media-font-10">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <li>{{ Auth::user()->name }}さん</li>
+                </a>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-            <li>{{ 'ログアウト' }}</li>
-            </a>
+                <li>{{ 'ログアウト' }}</li>
+                </a>
+            </ul>
+        </div>
         
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
             </form>
-        </ul>
-        <br>
-        <h1>アンパンマンの神経衰弱ゲーム</h1>
-        <ul>
-            <a href="{{ url('/') }}"><li class="gamemenu" id="noborder">トップページ(カード一覧）</li></a>
+    </div>
+        <ul class="flex-nsp media-font-10">
+            <a href="{{ url('/home') }}"><li class="gamemenu media-font-10" id="noborder">カード一覧</li></a>
             <a href="{{ route('game') }}"><li class="gamemenu">アンパンマン</li></a>
             <a class="nav-link" href="#"><li class="gamemenu">車</li></a>
             <a class="nav-link" href="#"><li class="gamemenu">山</li></a>
             <a class="nav-link" href="#"><li class="gamemenu">花</li></a>
-            <a class="nav-link" href="/anpanman/uploadForm"><li class="gamemenu">画像のアップロード</li></a>
+            <a class="nav-link" href="/anpanman/uploadForm"><li class="gamemenu">アップロード</li></a>
         </ul>
     </nav>
 @else
